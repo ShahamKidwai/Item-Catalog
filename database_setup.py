d43136218b7988f421b05c5db1b60489e2b8dfc5
skidwai2 <skidwai2@gmail.com>
2017-05-03 06:40:11 -0500

    feat: Add User Class in database

--- database_setup.py
+++ database_setup.py
@@ -7,11 +7,22 @@ from sqlalchemy import create_engine
 
 Base = declarative_base()
 
+
+class User(Base):
+    __tablename__= 'user'
+    id = Column(Integer, primary_key = True)
+    name = Column(String(250), nullable = False)
+    email = Column(String(250), nullable = True)
+    picture = Column(String(250), nullable = True)
+
+                    
 class Category(Base):
     __tablename__ = 'category'
     id = Column(Integer, primary_key = True)
     name = Column(String(250), nullable = False)
-
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+    
     @property
     def serialize(self):
         return {
@@ -25,8 +36,10 @@ class Item(Base):
     id = Column(Integer, primary_key = True)
     title = Column(String(250), nullable = False)
     description = Column(String(250), nullable=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
     category_id = Column(Integer, ForeignKey('category.id'))
     category = relationship(Category)
+    user = relationship(User)
 
     @property
     def serialize(self):
