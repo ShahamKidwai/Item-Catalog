9107872e3c4dd71d982725ade742d6944ce7f652
skidwai2 <skidwai2@gmail.com>
2017-05-03 03:09:59 -0500

    Add Comments

--- project.py
+++ project.py
@@ -1,7 +1,7 @@
 from time import strftime
 import os
 import jinja2
-from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
+from flask import Flask, render_template, request,redirect, url_for, flash, jsonify
 app = Flask(__name__)
 
 from sqlalchemy import create_engine
@@ -29,27 +29,36 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
+# this function creates a JSON endpoint to view Categories and the items
 @app.route('/JSON/')
 def catalogJSON():
     categories_all = session.query(Category).all()
     items_all = session.query(Item).all()   
     return jsonify(Category = [c.serialize for c in categories_all], Item = [i.serialize for i in items_all])
 
+# this function creates a JSON endpoint to view particular item information
+@app.route('/category/<string:category>/<string:item>/JSON')
+def showitemsJSON(category, item):
+    category = session.query(Category).filter_by(name=category).one()
+    item = session.query(Item).filter_by(title=item).one()
+    return jsonify(category = category.serialize, item = item.serialize)
 
+# below is a login function that renders login page and passes the state value into it
 @app.route('/login')
 def showLogin():
     state = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in xrange(32))
     login_session['state'] = state
     return render_template('login.html', STATE=state)
 
-
+# below function calls the gdisconnect function and redirects the user to home page
 @app.route('/logout')
 def logout():
     gdisconnect()
     Categories = session.query(Category).all()
     return render_template('publicCategories.html', cate = Categories) 
 
-
+# below function contains steps for obtaining authorization code for google
+# below function obtains authorization code in exchange of one time code
 @app.route('/gconnect', methods=['POST'])
 def gconnect():
     if request.args.get('state') != login_session['state']:
@@ -128,6 +137,7 @@ def gconnect():
     print "done!"
     return output
 
+# the below function is called to logout user from google account
 @app.route('/gdisconnect')
 def gdisconnect():
     access_token = login_session.get('access_token')
@@ -159,13 +169,7 @@ def gdisconnect():
     	response.headers['Content-Type'] = 'application/json'
     	return response
 
-@app.route('/category/<string:category>/<string:item>/JSON')
-def showitemsJSON(category, item):
-    category = session.query(Category).filter_by(name=category).one()
-    item = session.query(Item).filter_by(title=item).one()
-    return jsonify(category = category.serialize, item = item.serialize)
-
-
+# below function displays all the categories
 @app.route('/')
 @app.route('/categories')
 def showcategories():
@@ -177,7 +181,7 @@ def showcategories():
           islogin = True
     return render_template('ItemCategories.html', cate = Categories, isLogin = islogin)
 
-
+# below function allows the User to create a new category
 @app.route('/category/new/', methods = ['GET', 'POST'])
 def newCategory():
    if 'user_id' in login_session:
@@ -198,7 +202,7 @@ def newCategory():
    else:
        return render_template('newCategory.html')
 
-
+# below function displays items from specific category
 @app.route('/category/<int:category_id>/Items')
 @app.route('/category/<int:category_id>')
 def showitems(category_id):
@@ -218,6 +222,7 @@ def showitems(category_id):
                  islogin = True
                  return render_template('Items.html', category = category, cate = Categories, items = Items, isLogin = islogin)
 
+# below function displays specific item information from one particular category
 @app.route('/category/<int:category_id>/Items/<int:item_id>/')
 def ItemDescription(category_id, item_id):
     category = session.query(Category).filter_by(id = category_id).one()
@@ -236,6 +241,8 @@ def ItemDescription(category_id, item_id):
              else:
                   islogin = True
                   return render_template('ItemDescription.html', category = category, cate = Categories, item = itemone, items = Items, isLogin = islogin)
+
+# below function allows the user to update item name in a particular category
 
 @app.route('/category/<int:category_id>/Items/<int:item_id>/edit', methods = ['GET', 'POST'])
 def edititem(category_id, item_id):
@@ -265,6 +272,7 @@ def edititem(category_id, item_id):
     else:
          return render_template('item-edit.html', item = uitem, cate = Categories, category_id = category_id, cat = category, cats = cates, isLogin = islogin)
 
+# below function allows user to update item description from a particular category
 @app.route('/category/<int:category_id>/Items/<int:item_id>/editdescription', methods = ['GET', 'POST'])
 def editdescription(category_id, item_id):
     uitem = session.query(Item).filter_by(id = item_id, category_id = category_id).one()
@@ -287,9 +295,8 @@ def editdescription(category_id, item_id):
             return redirect(url_for('ItemDescription', category_id = category_id, item_id = item_id))
     else:
          return render_template('EditDescription.html', item = uitem, category_id = category.id, cate = cates, cat = category, isLogin = islogin)
-        
 
-
+# below function allows the user to delete item description 
 @app.route('/category/<int:category_id>/Items/<int:item_id>/deletedescription', methods = ['GET', 'POST'])
 def deletedescription(category_id, item_id):
     uitem = session.query(Item).filter_by(id = item_id, category_id = category_id).one()
@@ -312,7 +319,7 @@ def deletedescription(category_id, item_id):
     else:
          return render_template('DeleteDescription.html', item = uitem, cate = Categories, cat = category, category_id = category.id, isLogin = islogin)
         
-
+# below function allows user to create new item in a particular category
 @app.route('/category/<int:category_id>/Items/new',  methods = ['GET', 'POST'])
 def newitem(category_id):
      if 'user_id' in login_session:
@@ -334,7 +341,7 @@ def newitem(category_id):
           category = session.query(Category).filter_by(id = category_id).one()
           return render_template('NewItem.html', category_id = category_id, cate = categories, categories = categories, cat = category, isLogin = islogin)
         
-
+# below function allows user to delete item in a particular category
 @app.route('/category/<int:category_id>/Items/<int:item_id>/delete', methods = ['GET', 'POST'])
 def delete_item(category_id, item_id):
     itemToDelete = session.query(Item).filter_by(id = item_id).one()
@@ -355,11 +362,12 @@ def delete_item(category_id, item_id):
     else:
          return render_template('DeleteItem.html', i = itemToDelete, cate = Categories, isLogin = islogin)
 
-
+# below function takes user_id as input and returns the user object 
 def getUserInfo(user_id):
     user = session.query(User).filter_by(id = user_id).one()
     return user
 
+# below function takes user email as input and returns the user id 
 def getUserID(email):
     try:
         user = session.query(User).filter_by(email=email).one()
@@ -367,18 +375,13 @@ def getUserID(email):
     except:
         return None
 
-
+# below function extracts user info from login session and stores it in a user table
 def createUser(login_session):
     newUser = User(name = login_session['username'], email = login_session['email'], picture = login_session['picture'])
     session.add(newUser)
     session.commit()
     user = session.query(User).filter_by(email = login_session['email']).one()
     return user.id
-
-def respondToClient(description, status_code):
-    response = make_response(json.dumps(description), status_code)
-    response.headers['Content-Type'] = 'application/json'
-    return response
 
 if __name__ == '__main__':
      app.secret_key = 'super secret key'
